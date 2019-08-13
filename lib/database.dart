b075a9f2217290680598cba8e06a770331862f6a
Tantan4321 <kupa528@yahoo.com>
2019-08-13 15:07:20 -0500

    Update dependencies and configs to most recent firebase and flutter

--- lib/database.dart
+++ lib/database.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Counter {
@@ -13,72 +12,6 @@ abstract class Database {
   Future<void> setCounter(Counter counter);
   Future<void> deleteCounter(Counter counter);
   Stream<List<Counter>> countersStream();
-}
-
-// Realtime Database
-class AppDatabase implements Database {
-  Future<void> createCounter() async {
-    int now = DateTime.now().millisecondsSinceEpoch;
-    Counter counter = Counter(id: now, value: 0);
-    await setCounter(counter);
-  }
-
-  Future<void> setCounter(Counter counter) async {
-    DatabaseReference databaseReference = _databaseReference(counter);
-    await databaseReference.set(counter.value);
-  }
-
-  Future<void> deleteCounter(Counter counter) async {
-    DatabaseReference databaseReference = _databaseReference(counter);
-    await databaseReference.remove();
-  }
-
-  DatabaseReference _databaseReference(Counter counter) {
-    var path = '$rootPath/${counter.id}';
-    return FirebaseDatabase.instance.reference().child(path);
-  }
-
-  Stream<List<Counter>> countersStream() {
-    return _DatabaseStream<List<Counter>>(
-      apiPath: rootPath,
-      parser: _DatabaseCountersParser(),
-    ).stream;
-  }
-
-  static final String rootPath = 'counters';
-}
-
-class _DatabaseStream<T> {
-  _DatabaseStream({String apiPath, DatabaseNodeParser<T> parser}) {
-    FirebaseDatabase firebaseDatabase = FirebaseDatabase.instance;
-    DatabaseReference databaseReference =
-        firebaseDatabase.reference().child(apiPath);
-    var eventStream = databaseReference.onValue;
-    stream = eventStream.map((event) => parser.parse(event));
-  }
-
-  Stream<T> stream;
-}
-
-abstract class DatabaseNodeParser<T> {
-  T parse(Event event);
-}
-
-class _DatabaseCountersParser implements DatabaseNodeParser<List<Counter>> {
-  List<Counter> parse(Event event) {
-    Map<dynamic, dynamic> values = event.snapshot.value;
-    if (values != null) {
-      Iterable<String> keys = values.keys.cast<String>();
-
-      var counters = keys
-          .map((key) => Counter(id: int.parse(key), value: values[key]))
-          .toList();
-      counters.sort((lhs, rhs) => rhs.id.compareTo(lhs.id));
-      return counters;
-    } else {
-      return [];
-    }
-  }
 }
 
 // Cloud Firestore
